15cfd0e37813ff3635ee371eba6e2ad24daf8a3b
wandumi <munandih@gmail.com>
2022-12-26 01:09:57 +0200

    Auth Working

---
+++ README.md
@@ -0,0 +1,34 @@
+## About The Application
+
+The application accesses the Hacker News API (stories, comments, best stories) and store them in the database
+### How to Install
+
+Clone the project <br />
+```Install npm ``` and
+```composer update```
+
+Duplicate the file .env_example to .env and change the
+database connections on the following <br /><br />
+```DB_CONNECTION=mysql```<br />
+```DB_HOST=127.0.0.1```<br />
+```DB_PORT=3306```<br />
+```DB_DATABASE=Database_name```<br />
+```DB_USERNAME=root```<br />
+```DB_PASSWORD=```<br />
+
+Run Migration with the following Command <br /><br />
+```php artisan migrate```
+
+Generate the key for the application
+
+```key generate```
+
+Start the server with
+
+``php artisan serve``
+
+### Developer
+
+Wandumi Munandi Sichali <br />
+Full Stack Developer 
+
---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterRequest;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        if(!Auth::attempt($request->only('email','password')))
+        {
+            return response([
+                'error' => 'Invalid Credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        // $user = Auth::user();
+
+        return response([
+            'message' => 'success ',
+            
+        ]);
+
+
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create(
+            $request->only('first_name','last_name','email')+[
+                'password' => \Hash::make($request->input('password')),
+            ]
+        );
+
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function user(Request $request)
+    {
+         return $request->user();
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        return response()->json('', 204);
+    }
+}
